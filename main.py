a670e715dc44f3eb325be948aa3c1680e7f695f9
wandy <wandywahidin53@gmail.com>
2021-08-26 20:52:48 +0700

    tester percobaan

---
+++ main.py
@@ -0,0 +1,4 @@
+print('ini adalah project python pertama saya dengan menggunakan laptop baru')
+print('pertama kali menggunakan pycharm untuk code editor')
+print('mudah-mudahan bisa lancar belajar nya')
+print('aminnn.')
